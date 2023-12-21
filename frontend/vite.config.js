72497d7d71c7bebc6601aea9456ddbddcf9030ef
AB27-source <angadbhatti18@gmail.com>
2023-12-20 17:00:57 -0800

    moved vite config to frontend directory

---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:5173',
+  },
+  test: {
+    coverage: {
+      reporter: ['text', 'cobertura'],
+      reportsDirectory: './coverage',
+    },
+  },
+});
