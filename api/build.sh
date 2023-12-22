4ce78e4ddf50f1d5f2cdee4f96ec8fd4f325c697
AB27-source <angadbhatti18@gmail.com>
2023-12-21 16:23:41 -0800

    modified python version

--- api/build.sh
+++ api/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
-python3.10 -m pip install --upgrade pip
-python3.10 -m pip install -r requirements.txt
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
 
 echo "Migrating database..."
 
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Creating superuser..."
 
@@ -15,8 +15,8 @@ export DJANGO_SUPERUSER_EMAIL=${SUPERUSER_EMAIL}
 export DJANGO_SUPERUSER_USERNAME=${SUPERUSER_USERNAME}
 export DJANGO_SUPERUSER_PASSWORD=${EMAIL_HOST_PASSWORD}
 
-python3.10 manage.py createsuperuser --noinput
+python3.9 manage.py createsuperuser --noinput
 
 echo "Collecting static files..."
 
-python3.10 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
