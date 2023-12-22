63c35a403c5c97267207554367396a4ad58e2cd6
AB27-source <angadbhatti18@gmail.com>
2023-12-21 16:05:43 -0800

    changed build script

--- api/build.sh
+++ api/build.sh
@@ -16,7 +16,6 @@ export DJANGO_SUPERUSER_USERNAME=${SUPERUSER_USERNAME}
 export DJANGO_SUPERUSER_PASSWORD=${EMAIL_HOST_PASSWORD}
 
 python3.10 manage.py createsuperuser --noinput
-python3.10 manage.py runscript load_courses
 
 echo "Collecting static files..."
 
